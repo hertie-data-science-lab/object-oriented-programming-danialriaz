3112f0b6368c456677fafb4a738cbc8a7d1c6bd3
Danial Riaz <danialriaz91@gmail.com>
2023-02-19 12:13:42 +0100

    Updated

--- Creatures.py
+++ Creatures.py
@@ -7,14 +7,56 @@ Created on Sun Feb 12 18:02:49 2023
 
 from abc import ABCMeta, abstractmethod
 import numpy as np
+import random
 
 class Creature(metaclass=ABCMeta):
     
+    def __init__(self,ind):
+        self.ind = ind #Index of a creature in ecosystem
+        self.type = "None"
+    
+    @abstractmethod 
+    def move(self, river, i):
+        pass
   
         
 class Bear(Creature):
-
- 
+  
+    def __init__(self, ind):
+       self.type = "bear"
+    
+    def move(self, river, i):
+        new_i = random.choice([i-1, i, i+1])
+        if new_i >= 0 and new_i < len(river):
+            if river[new_i] is None:
+                river[new_i] = self
+                river[i] = None
+            elif river[new_i].type == "bear":
+                new_location = random.choice([i-1, i, i+1])
+                if new_location >= 0 and new_location < len(river) and river[new_location] is None:
+                    river[new_location] = Bear(new_location)
+            elif river[new_i].type == "fish":
+                river[i] = None
+                river[new_i] = self
         
 class Fish(Creature):
     
+    def __init__(self, ind):
+        self.type = "fish"
+
+    def move(self, river, i):
+        new_i = random.choice([i-1, i, i+1])
+        if new_i >= 0 and new_i < len(river):
+            if river[new_i] is None:
+                river[new_i] = self
+                river[i] = None
+            elif river[new_i].type == "fish":
+                new_location = random.choice([i-1, i, i+1])
+                if new_location >= 0 and new_location < len(river) and river[new_location] is None:
+                    river[new_location] = Fish(new_location)
+            elif river[new_i].type == "bear":
+                river[i] = None
+                
+                
+                
+                
--- River.py
+++ River.py
@@ -5,17 +5,47 @@ Created on Sun Feb 12 18:04:03 2023
 @author: Hannah
 """
 import numpy as np
+import random
 from Creatures import Bear
 from Creatures import Fish
 
 class River:
     
     def __init__(self, n_room):
+        self.n_room = n_room
+        self.list = [None] * n_room
        
-        
+    def initialize(self):
+        for i in range(self.n_room):
+            random_number = random.randint(0, 2)
+            if (random_number == 0):
+                self.list[i] = Fish(i)
+            elif (random_number == 1):
+                self.list[i] = Bear(i)
+            elif (random_number == 2):
+                pass;               
        
     def display(self):
         print("===================")
         print("Ecosystem status:\n")
-        print(self.eco, "\n")
+        for i in self.list: 
+            if i == None:       
+                print("None")
+            else: 
+                print(i.type)
         print("===================")
+        
+    def next_time_step(self, no_iter):
+        for t in range(no_iter):
+            # loop over each position in the river
+            for i in range(len(self.list)):
+                # get the animal at this position, if there is one
+                animal = self.list[i]
+
+                if animal is not None:
+                    # move the animal to a random adjacent position
+                    animal.move(self.list, i)
+        
+
+        
+        
